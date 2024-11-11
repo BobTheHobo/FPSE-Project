429620633ec8a3e09f0238488bec078d5e3035d7
bathan1 <side_nathan@hotmail.com>
2024-11-11 15:15:09 -0500

    get backend to call lib game functions

--- backend/src/bin/server.ml
+++ backend/src/bin/server.ml
@@ -1,26 +1,37 @@
 open Core
 open Dream
+open Game
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 type obstacle_object = {
   obstacles : (int * int) list
 } [@@deriving yojson]
 
+let random_el set =
+  let len = Set.length set in
+  Set.nth set (Random.int len)
+  |> function
+    | Some el -> el
+    | None -> failwith "lol"
 
-let update_obstacles obstacles : (int * int) list =
-  let (x, y) : (int * int) = match List.last obstacles with
-  | Some(move) -> move
-  | None -> (5, 5)
-  in 
-  let next_move : (int * int) = 
-    let choice : int = Random.int 3
-    in 
-    if choice = 0 then (x + 1, y)
-    else if choice = 1 then (x, y + 1)
-    else if choice = 2 then (x - 1, y)
-    else (x, y - 1)
-  in
-  obstacles @ [next_move]
+let init_obstacles ~width ~height =
+  let first = { Simple_grid.x = Random.int width; y = Random.int height } in
+  let surrounding = Simple_grid.neighbors first ~width ~height in
+  let second = random_el surrounding in
+  let removed = Set.remove surrounding second in
+  let third = random_el removed in
+  [ first; second; third ]
+  
+let coordinate_to_pair_list (ls : Simple_grid.coordinate list) = List.map ls ~f:(fun { x; y } -> (x, y ))
+let pair_to_coordinate_list (pairs : (int * int) list) = List.map pairs ~f:(fun (x, y) -> { Simple_grid.x; y })
+
+let get_next_obstacles obstacles : Simple_grid.coordinate list =
+  match obstacles with
+  | [] -> init_obstacles ~width:10 ~height:10
+  | hd ->
+    let set = Simple_grid.Coordinate_set.of_list hd in
+    let updated = Simple_grid.next { Simple_grid.cells = set; width = 10; height = 10 } in
+    Set.to_list updated.cells 
 
 let () =
   run
@@ -35,7 +46,10 @@ let () =
             |> Yojson.Safe.from_string
             |> obstacle_object_of_yojson
           in
-          update_obstacles obstacle_object.obstacles
+          obstacle_object.obstacles
+          |> pair_to_coordinate_list
+          |> get_next_obstacles
+          |> coordinate_to_pair_list
           |> List.map ~f:(fun (x, y) -> Printf.sprintf "[%d, %d]" x y)
           |> (fun a -> String.concat a ~sep:", ")
           |> Printf.sprintf "[%s]"
