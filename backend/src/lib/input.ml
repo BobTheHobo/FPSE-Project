8b28e1baede37a80e9ccb94cc7383e31e9274c81
nathan <side_nathan@hotmail.com>
2024-11-19 18:57:50 -0500

    fix input and cli

--- backend/src/lib/input.ml
+++ backend/src/lib/input.ml
@@ -1,29 +1,31 @@
 open Core
 
+module Base_grid = Grid.Make(Base_cell)
+
 let example_cells =
-  Grid.Coordinate_set.of_list
+  Base_grid.Coordinate_set.of_list
     [ { x = 0; y = 1 }; { x = 0; y = 4 }; { x = 2; y = 2 }; { x = 2; y = 3 } ]
 
-let example_grid = { Grid.cells = example_cells; width = 5; height = 5 }
+let example_grid = { Base_grid.cells = example_cells; width = 5; height = 5 }
 
 let blinker_cells =
-  Grid.Coordinate_set.of_list
+  Base_grid.Coordinate_set.of_list
     [ { x = 2; y = 1 }; { x = 2; y = 2 }; { x = 2; y = 3 } ]
 
-let blinker_grid = { Grid.cells = blinker_cells; width = 5; height = 5 }
+let blinker_grid = { Base_grid.cells = blinker_cells; width = 5; height = 5 }
 
 module GridVis = Vis.MakeGridVis (Vis.ConwayCellVis)
 
-let rec input_loop (grid : Grid.t) : unit =
+let rec input_loop (grid : Base_grid.t) : unit =
   Out_channel.(flush stdout);
   let inp : string option = In_channel.(input_line stdin) in
-  let result (grid : Grid.t) : Grid.t =
+  let result (grid : Base_grid.t) : Base_grid.t =
     match inp with
     | Some "w" ->
         print_string "going up\n";
         grid
     | Some "n" ->
-        let next_grid = Grid.next grid in
+        let next_grid = Base_grid.next grid in
         GridVis.draw_grid next_grid;
         next_grid
     | Some "help" ->
