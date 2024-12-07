231b624d3bbc212776fe55eb25dee7b1dff8dc5e
nathan <side_nathan@hotmail.com>
2024-12-06 21:16:44 -0500

    get game to initialize

--- backend/src/bin/server.ml
+++ backend/src/bin/server.ml
@@ -18,6 +18,18 @@ module Base_game = Grid.Make (Key)
 type obstacle_object = { fire : (int * int) list; ice : (int * int) list; player : (int * int) }
 [@@deriving yojson]
 
+type params_json = {
+  b: int;
+  s1: int;
+  s2: int;
+} [@@deriving yojson]
+
+type game_params = {
+  fire : params_json;
+  ice : params_json;
+  water : params_json;
+} [@@deriving yojson]
+
 let coordinate_to_pair_list (ls : Key.t list) : (int * int) list =
   List.map ls ~f:(fun { Key.x; y } -> (x, y))
 
@@ -86,6 +98,94 @@ let get_next_board (fire_state : (int * int) list) (ice_state : (int * int) list
   let fire_encoded = encode_set next_fire_state in
   let ice_encoded = encode_set next_ice_state in
   Printf.sprintf "{ \"fire\": [%s], \"ice\": [%s], \"player\": %s }" fire_encoded ice_encoded (encode_pair (player_pos is_dead (player_x, player_y)))
+  
+module Cell_type : Map_grid.CELL_TYPE = struct
+  module T = struct
+    type t =
+      | Fire 
+      | Ice 
+      | Water [@@deriving sexp, compare]
+  end
+  include T
+  module TSet = Set.Make(T)
+  
+  let to_string (t : t) = Sexp.to_string (sexp_of_t t)
+  let type_list = [T.Fire; T.Ice; T.Water] 
+  let compare = T.compare
+  let params_of_t (t : t) = match t with
+    | Fire -> { Map_grid.Params.b = 3; s1 = 2; s2 = 3 }
+    | Ice -> { b = 3; s1 = 2; s2 = 3 }
+    | Water -> { b = 3; s1 = 2; s2 = 3 }
+    
+  let handle_collision (a : t) (b : t) = match (a, b) with
+    | (Fire, Ice) -> Some Water
+    | (Ice, Water) -> Some Ice
+    | _ -> None
+    
+  let handle_collisions (tset : TSet.t) = match Set.to_list tset with
+    | [] -> None
+    | a :: [] -> Some a
+    | a :: b :: [] -> handle_collision a b
+    | a :: b :: c :: [] -> (
+      match handle_collision a b with
+      | None -> Some c
+      | Some cell -> handle_collision cell c
+    )
+    | _ -> None
+end
+
+module Game_grid = Map_grid.Make(Cell_type)
+let fire = match Cell_type.type_list with
+| fire :: _ -> fire
+| _ -> failwith "lol"
+
+let ice = match Cell_type.type_list with
+| _ :: ice :: _ -> ice
+| _ -> failwith "lol"
+
+let fire_set = Cell_type.TSet.of_list [fire]
+let ice_set = Cell_type.TSet.of_list [ice]
+
+let coordinate_to_assoc (coordinate : Map_grid.Coordinate.t) =
+  `Assoc [
+    ("x", `Int coordinate.x);
+    ("y", `Int coordinate.y)
+  ]
+  
+let encode_map_grid (map_grid : Game_grid.t) =
+  let map_as_list =
+    Map.fold map_grid ~init:[] ~f:(fun ~key ~data acc ->
+      let cell_type_list = Set.to_list data |> List.map ~f:(Cell_type.to_string) in
+      let entry = `Assoc [
+        ("coordinate", coordinate_to_assoc key);
+        ("cellTypes", `List (List.map cell_type_list ~f:(fun ct -> `String ct)))
+      ] in
+      entry :: acc
+    )
+  in 
+  `List map_as_list
+  
+let encode_game_state (map_grid : Game_grid.t) (player : Map_grid.Coordinate.t) : string =
+  let json = `Assoc [
+    ("obstacles", encode_map_grid map_grid);
+    ("player", coordinate_to_assoc player)
+  ] in
+  Yojson.Safe.to_string json
+
+let init_game =
+  let random_fire_cell_positions = [
+    { Map_grid.Coordinate.x = 5; y = 4 };
+    { Map_grid.Coordinate.x = 4; y = 5 };
+    { Map_grid.Coordinate.x = 6; y = 5 };
+  ] in
+  let random_ice_cell_positions = [
+    { Map_grid.Coordinate.x = 10; y = 9 };
+    { Map_grid.Coordinate.x = 9; y = 10 };
+    { Map_grid.Coordinate.x = 11; y = 10 };
+  ] in
+  let mapped_to_fire = List.map random_fire_cell_positions ~f:(fun c -> (c, fire_set)) in
+  let mapped_to_ice = List.map random_ice_cell_positions ~f:(fun c -> (c, ice_set)) in
+  Game_grid.CMap.of_alist_exn (mapped_to_fire @ mapped_to_ice)
 
 let () =
   run 
@@ -113,4 +213,21 @@ let () =
             ("Access-Control-Allow-Headers", "*");
           ]
         "");
+      post "/game" (fun _ ->
+        init_game
+        |> fun state -> encode_game_state state { x = 0; y = 0 }
+        |> respond ~headers:
+          [
+            ("Access-Control-Allow-Origin", "*");
+            ("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            ("Access-Control-Allow-Headers", "Content-Type");
+            ("Content-Type", "application/json");
+        ]);
+      options "/game" (fun _ ->
+        respond ~headers:
+          [
+            ("Access-Control-Allow-Origin", "*");
+            ("Access-Control-Allow-Headers", "*");
+          ]
+      "");
     ]
--- backend/test/tests.ml
+++ backend/test/tests.ml
@@ -168,49 +168,6 @@ let test_all_dead_from_solitude_quickcheck _ =
   let mnext = M_grid.next m ~height ~width in
   assert_equal (Map.length mnext) 0
 
-let loop (grid : Grid.t) (iter_limit : int) ~(f : Grid.t -> int -> unit) : unit
-    =
-  let rec next (prev : Grid.t) (n : int) =
-    if n = iter_limit then ()
-    else (
-      f prev n;
-      next (Grid.next prev) (n + 1))
-  in
-  next grid 0
-
-let test_simple_next _ =
-  let previous_cells =
-    Grid.Coordinate_set.of_list
-      [
-        { Cell.x = 0; y = 1 };
-        { x = 0; y = 4 };
-        { x = 2; y = 2 };
-        { x = 2; y = 3 };
-      ]
-  in
-  let prev = { Grid.cells = previous_cells; width = 5; height = 5 } in
-  let next_grid = Grid.next prev in
-  printf "Prev cells is %s\n"
-    (Sexp.to_string (Grid.Coordinate_set.sexp_of_t prev.cells));
-  printf "Next cells is %s\n"
-    (Sexp.to_string (Grid.Coordinate_set.sexp_of_t next_grid.cells))
-
-(* Tests a blinker which is an oscillating pattern, see link below for gif *)
-(* https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#/media/File:Game_of_life_blinker.gif *)
-let test_blinker _ =
-  let blinker_cells =
-    Grid.Coordinate_set.of_list
-      [ { x = 2; y = 1 }; { x = 2; y = 2 }; { x = 2; y = 3 } ]
-  in
-  let grid = { Grid.cells = blinker_cells; width = 5; height = 5 } in
-
-  loop grid 10 ~f:(fun prev n ->
-      let str = Sexp.to_string (Grid.Coordinate_set.sexp_of_t prev.cells) in
-      (* printf "%d : %s" n str; *)
-      if n mod 2 = 0 then
-        assert_equal "(((x 2)(y 1))((x 2)(y 2))((x 2)(y 3)))" str
-      else assert_equal "(((x 1)(y 2))((x 2)(y 2))((x 3)(y 2)))" str)
-
 let example_suite =
   "Example suite"
   >::: [
@@ -218,8 +175,6 @@ let example_suite =
         "test corners_alive_overpopulation example" >:: test_corners_alive_overpopulation_example;
         "test test_spawn_example" >:: test_spawn_example;
         "test test_fire_ice_collision" >:: test_fire_ice_collision_example;
-        "test simple next" >:: test_simple_next;
-        "test blinker 10 iterations" >:: test_blinker;
        ];;
     
 let quickcheck_suite =
